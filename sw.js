ad5c58056e282f403ac286dfababea1b42f893d2
Anbarasan <anbarasanj24@gmail.com>
2021-11-14 23:38:52 +0530

    Added cache version, cache size and fallback page

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
     '/',
     '/index.html',
+    '/pages/fallback.html',
     '/js/app.js',
     '/js/ui.js',
     '/js/materialize.min.js',
@@ -11,7 +13,17 @@ const assets = [
     'https://fonts.googleapis.com/icon?family=Material+Icons'
 ]
 
+// cache size
+const limitCacheSize = (name, size) => {
 
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if (keys.length > size) {
+                cache.delete(keys[0]).then(limitCacheSize(name, size))
+            }
+        })
+    })
+}
 
 // Install event on service worker
 self.addEventListener('install', event => {
@@ -24,9 +36,18 @@ self.addEventListener('install', event => {
     );
 })
 
+
 // Active event on service worker 
-self.addEventListener('active', event => {
-    console.log("Service Worker is activated")
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        // cache keys will return cache names as site-static, site-static-v1
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+                .map(key => caches.delete(key))
+            )
+        })
+    )
 })
 
 
@@ -35,7 +56,18 @@ self.addEventListener('fetch', event => {
     // console.log("Fetch events", event)
     event.respondWith(
         caches.match(event.request).then(cacheRes => {
-            return cacheRes || fetch(event.request);
+
+            return cacheRes || fetch(event.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(event.request.url, fetchRes.clone());
+                    limitCacheSize(dynamicCacheName, 2);
+                    return fetchRes;
+                })
+            });
+        }).catch(() => {
+            if (event.request.url.indexOf('.html') > -1) {
+                return caches.match('/pages/fallback.html');
+            }
         })
     )
 })
